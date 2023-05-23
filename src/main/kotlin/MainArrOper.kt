e99b95073fbc0b4a290c7a449afb36cf73d925d6
Boris R <borisrozhkin@gmail.com>
2023-05-24 00:37:18 +0500

    Change exceptions message

--- src/main/kotlin/MainArrOper.kt
+++ src/main/kotlin/MainArrOper.kt
@@ -2,8 +2,8 @@ import ots.complex.*
 import java.util.*
 
 fun main(args: Array<String>) {
-    val ar1 = arrayOf( 2.R)
-    val ar2 = arrayOf( 20.R )
+    val ar1 = arrayOf( 2.R, 3.R)
+    val ar2 = arrayOf( 20.R, 10.R )
     println("ar1=${Arrays.deepToString(ar1)} ar2=${Arrays.deepToString(ar2)} ar1+ar2=${Arrays.deepToString(ar1+ar2)} ")
     println("ar1=${Arrays.deepToString(ar1)} ar2=${Arrays.deepToString(ar2)} ar1*ar2=${Arrays.deepToString(ar1*ar2)} ")
     println("ar1=${Arrays.deepToString(ar1)} ar2=${Arrays.deepToString(ar2)} ar1-ar2=${Arrays.deepToString(ar1-ar2)} ")
