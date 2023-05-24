0409f9082f2a1540c4cb70906d141f0402debdb7
Paranin Alexander <alexander.paranin@ecrt.ru>
2023-05-24 15:41:04 +0500

    добавил коментарий к evalAXFind умножение массива на число

--- src/main/kotlin/MainArrOper.kt
+++ src/main/kotlin/MainArrOper.kt
@@ -4,11 +4,12 @@ import java.util.*
 fun main(args: Array<String>) {
     val ar1 = arrayOf( 2.R, 3.R)
     val ar2 = arrayOf( 20.R, 10.R )
+
     println("ar1=${Arrays.deepToString(ar1)} ar2=${Arrays.deepToString(ar2)} ar1+ar2=${Arrays.deepToString(ar1+ar2)} ")
     println("ar1=${Arrays.deepToString(ar1)} ar2=${Arrays.deepToString(ar2)} ar1*ar2=${Arrays.deepToString(ar1*ar2)} ")
     println("ar1=${Arrays.deepToString(ar1)} ar2=${Arrays.deepToString(ar2)} ar1-ar2=${Arrays.deepToString(ar1-ar2)} ")
     println("ar1=${Arrays.deepToString(ar1)} ar2=${Arrays.deepToString(ar2)} ar1/ar2=${Arrays.deepToString(ar1/ar2)} ")
-
+    println("ar1=${Arrays.deepToString(ar1)}  ar1*2.0.R=${Arrays.deepToString(ar1*2.0.R)} ")
 
 
 }
