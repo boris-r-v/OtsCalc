52cfc4c75ed49def2339de412732c2fe3cae0e14
Boris R <borisrozhkin@gmail.com>
2023-08-29 21:27:49 +0500

    Добавил алгоритмы пост обрботки данных расчета ОТС:  1. Определние доли времени в течении которого напряжение земля-рельс были больше максиально допустимого напряжения прикосновения  2. Статистически корридор для напряжения в полторы сигмы  3. Массив максимальных значений обратного тока в узлах сетки и изменил алгоримт расчета действущего значения (оконный давал некоректные значения)

--- src/main/kotlin/ots/statistic/Data.kt
+++ src/main/kotlin/ots/statistic/Data.kt
@@ -2,6 +2,7 @@ package ots.statistic
 
 import ots.calc.Track
 import java.io.File
+import kotlin.math.absoluteValue
 import kotlin.math.pow
 import kotlin.math.sqrt
 
@@ -14,8 +15,12 @@ import kotlin.math.sqrt
  * @property minU массив минимальны значений напряжений в узлах сетки
  * @property avrU массив средних арифметических значений напряжений в узлах сетки
  * @property skoU массив среднеквадратичных отклонений от среднего значения напряжений у узлах сетки
- * @property posAvrU массив положительных средних арифметических значений напряжений в узлах сетки
- * @property rmsI действующее значение тока за rms_steps шагов расчета в каждом узле сетки
+ * @property posAvrU массив положительных средних арифметических значений напряжений в узлах сетки, полезно для оценки потенциальной опасности электрокоррозии рельсов и железобетонных контрукций
+ * @property exContactU доля времени (в %) когда было приевышено максимальное напряжения прикосновения к рельсу
+ * @property maxSU массив максимальных значений напряжений статистического корридора в узлах сетки, значение напряжения попавшие в диапаон (avrU, avrU+1.5*skoU]
+ * @property minSU массив минимальны значений напряжений статистического корридора в узлах сетки, значение напряжения попавшие в диапаон (avrU, avrU-1.5*skoU]
+ * @property maxI массив максимальных значений тока в каждом узле сетки
+ * @property rmsI массив действующих значение тока за rmsWindow шагов расчета в каждом узле сетки
  */
 
 @Suppress("NAME_SHADOWING")
@@ -26,8 +31,16 @@ class TrackStat(track: Track, rmsWindow: Int) {
     internal val avrU: Array<Double> = Array(size) {0.0}
     internal val skoU: Array<Double> = Array(size) {0.0}
     internal val posAvrU: Array<Double> = Array(size) {0.0}
+    internal val exContactU: Array<Double> = Array(size) {0.0}
+    internal val maxSU: Array<Double> = Array(size) {0.0}
+    internal val minSU: Array<Double> = Array(size) {0.0}
     internal val rmsI: Array<Double> = Array(size) {0.0}
-
+    internal val maxI: Array<Double> = Array(size) {0.0}
+    /**
+     * Допустимая величина потенцила рельсов,
+     * равно допустимое напряжение прикосновения на теле человека (75 В) деленоек на коэффициент прикосновения (примерно 0.5)
+     */
+    private val railMaxAllowedU: Double = 75.0/0.5
     /**
      * В методе инициализации проходит вся магия приготовления/расчета статистики
      */
@@ -38,6 +51,7 @@ class TrackStat(track: Track, rmsWindow: Int) {
             var min = arrForOneMeshPoint[0].mod
             var max = arrForOneMeshPoint[0].mod
             val size = arrForOneMeshPoint.size
+            var moreThenAllowedU = 0
             arrForOneMeshPoint.forEach{ real ->
                 val mod = real.mod
                 if (mod < min)
@@ -47,35 +61,64 @@ class TrackStat(track: Track, rmsWindow: Int) {
                 sumOfUArray += mod
                 if (mod > 0)
                     sumOfPosUArray += mod
-
+                if ( mod.absoluteValue > railMaxAllowedU ){
+                    moreThenAllowedU += 1
+                }
             }
+
             maxU[i] = max
             minU[i] = min
             avrU[i] = sumOfUArray / size
             posAvrU[i] = sumOfPosUArray / size
+            exContactU[i] = 100.0 * moreThenAllowedU / size
             val arv = avrU[i]
             var sumOfDiffSq = 0.0
             arrForOneMeshPoint.forEach { real ->
                 sumOfDiffSq += (arv - real.mod).pow(2.0)
             }
             skoU[i] = sqrt( sumOfDiffSq / size )
+            maxSU[i] = avrU[i] + 1.5 * skoU[i]
+            minSU[i] = avrU[i] - 1.5 * skoU[i]
         }
         track.histI.forEachIndexed { i, arrForOneMeshPoint ->
+
+            maxI[i] = arrForOneMeshPoint[0].mod.absoluteValue
+            var sumOfSquare = 0.0
+            arrForOneMeshPoint.forEach{ real ->
+                val mod = real.mod.absoluteValue
+                if (mod > maxI[i]){
+                    maxI[i] = mod
+                }
+                sumOfSquare += mod.pow(2.0)
+            }
+            rmsI[i] = sqrt( sumOfSquare/arrForOneMeshPoint.size)
+        /*
+            This RMS windows algo gives rmsI value more then maxI
+            maxI[i] = arrForOneMeshPoint[0].mod.absoluteValue
             var rms = 0.0
             val size = arrForOneMeshPoint.size
             if (rmsWindow >= size)
                 throw Exception("Размер окна расчета RMS меньше количества проведенных расчетов мгновенных схем")
-            for ( i in 0..rmsWindow){
-                rms += arrForOneMeshPoint[i].mod.pow(2.0)
+            for ( ii in 0..rmsWindow){
+                val mod = arrForOneMeshPoint[ii].mod
+                rms += mod.pow(2.0)
+                if (mod.absoluteValue > maxI[i]){
+                    maxI[i] = mod.absoluteValue
+                }
             }
             var maxRMS = rms
-            for ( i in rmsWindow+1 until size){
-                rms = rms - arrForOneMeshPoint[i - rmsWindow].mod.pow(2.0) + arrForOneMeshPoint[i].mod.pow(2.0)
+            for ( ii in rmsWindow+1 until size){
+                val mod = arrForOneMeshPoint[ii].mod
+                rms = rms - arrForOneMeshPoint[ii - rmsWindow].mod.pow(2.0) + mod.pow(2.0)
                 if (rms > maxRMS) {
                     maxRMS = rms
                 }
+                if (mod.absoluteValue > maxI[i]){
+                    maxI[i] = mod.absoluteValue
+                }
             }
-            rmsI[i] = sqrt( maxRMS )
+            rmsI[i] = sqrt( maxRMS/rmsWindow )
+        */
         }
     }
 }
@@ -98,6 +141,7 @@ class Data (tracks: Array<Track> ) {
         println("track${track}.posArvU: ${data[track].posAvrU.contentDeepToString()} " )
         println("track${track}.mimU: ${data[track].minU.contentDeepToString()} " )
         println("track${track}.skoU: ${data[track].skoU.contentDeepToString()} " )
+        println("track${track}.maxI: ${data[track].maxI.contentDeepToString()} " )
         println("track${track}.rmsI: ${data[track].rmsI.contentDeepToString()} " )
     }
 
@@ -107,10 +151,10 @@ class Data (tracks: Array<Track> ) {
      */
       fun write2csv( filePath: String, trackIdx: Int ){
          File(filePath).printWriter().use { out->
-             out.println("meshNode,maxU,minU,arvU,skoU,posAvrU,rmsI")
+             out.println("meshNode,maxU,minU,arvU,skoU,posAvrU,exContactU,maxSU,minSU,maxI,rmsI")
              val tr = data[trackIdx]
              for (i in 0 until tr.size){
-                 out.println("${i},${tr.maxU[i]},${tr.minU[i]},${tr.avrU[i]},${tr.skoU[i]},${tr.posAvrU[i]},${tr.rmsI[i]}")
+                 out.println("${i},${tr.maxU[i]},${tr.minU[i]},${tr.avrU[i]},${tr.skoU[i]},${tr.posAvrU[i]},${tr.exContactU[i]},${tr.maxSU[i]},${tr.minSU[i]},${tr.maxI[i]},${tr.rmsI[i]}")
              }
          }
          println("StatData for track${trackIdx} was saved to file $filePath")
